5a0a85867053c5604fe5445c114e805049b2a2fb
Supporto <cristian@brainplatform.it>
2025-05-22 17:58:52 +0200

    test_2

--- attivita/attivita_controller.py
+++ attivita/attivita_controller.py
@@ -12,6 +12,12 @@ def utenti(response: Response,):
 
     return attivita_services.get_all_utenti()
 
+@router.get("/test_2/")
+@router.get("/test_2", include_in_schema=False)
+def test_2(response: Response,):
+
+    return attivita_services.get_all_utenti()
+
 
 @router.post("/get_attivita/")
 @router.post("/get_attivita", include_in_schema=False)
