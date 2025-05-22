ab1f2bef552fa3c055be57406c2a0b192536e089
Supporto <cristian@brainplatform.it>
2025-05-22 16:19:40 +0200

    0.

--- api/router.py
+++ api/router.py
@@ -4,5 +4,5 @@ from attivita.attivita_controller import router as attivita_routes
 
 api_router = APIRouter()
 
-api_router.include_router(utenti_routes, prefix="/api/utenti", tags=["utenti"])
+# api_router.include_router(utenti_routes, prefix="/api/utenti", tags=["utenti"])
 api_router.include_router(attivita_routes, prefix="/api/attivita", tags=["attivita"])
